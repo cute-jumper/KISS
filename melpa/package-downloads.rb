3ac563d20829f7c2d02ab40f6508081bd2a43106
Junpeng Qiu <qjpchmail@gmail.com>
2015-04-20 00:59:31 -0400

    Add fcitx and fix nil problem

--- melpa/package-downloads.rb
+++ melpa/package-downloads.rb
@@ -13,20 +13,25 @@ old_data = File.exist?($DATA_FILE) ? JSON.load(open($DATA_FILE)) : nil
 package_names = JSON.load(open($BASE_URL + 'recipes.json')).keys &
                 JSON.load(open($BASE_URL + 'archive.json')).keys
 
-puts "Total packages: #{package_names.length}"
+puts "Total packages: #{package_names.length}" + (old_data ?
+                                                    "(#{old_data['Total packages']})" : "")
 data = {"Total packages" => package_names.length}
 data['my packages'] = {}
 
 downloads = JSON.load(open($BASE_URL + 'download_counts.json'))
 
-my_packages = %w(gscholar-bibtex ace-flyspell ace-pinyin) + ARGV
+package_names &= downloads.keys
+
+my_packages = %w(gscholar-bibtex ace-flyspell ace-pinyin ace-jump-helm-line fcitx) + ARGV
+
 my_packages.each do |pkg_name|
   d = downloads[pkg_name]
+  next if d.nil?
   below_number = package_names.select do |name|
     downloads[name] < d
   end.length
   percent = below_number * 100.0 / package_names.length
-  old_stats = old_data['my packages'][pkg_name] if old_data['my packages']
+  old_stats = old_data['my packages'][pkg_name] if old_data and old_data['my packages']
   if old_stats
     puts "#{pkg_name}\n\tdownloads: #{d}(#{old_stats['downloads']}), rank: #{below_number + 1}(#{old_stats['rank']}), \
 percentile: %.2f(#{old_stats['percentile']})" % percent
