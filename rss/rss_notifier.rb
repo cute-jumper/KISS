be30dde06e77e9bedd495e92a7f15acafaf87ee7
Junpeng Qiu <qjpchmail@gmail.com>
2016-01-09 20:55:44 -0500

    Add rss notifier

---
+++ rss/rss_notifier.rb
@@ -0,0 +1,42 @@
+#! /usr/bin/env ruby
+#-*- coding: utf-8 -*-
+# Author: Junpeng Qiu
+# Date: <2016-01-08 Fri>
+# Description: RSS Notifier in Ruby
+
+require 'rss'
+require 'open-uri'
+require 'libnotify'
+require 'eventmachine'
+
+$RSS_URLS = %w(http://rss.cnbeta.com/rss)
+$INTERVAL = 60 * 30
+$TIMEOUT = 10
+$ICON_PATH  = "#{File.join(File.expand_path(File.dirname(__FILE__)), 'rss_notifier_icon.png')}"
+
+$CACHE = $RSS_URLS.map { |e| [e,[]] }.to_h
+
+def show_rss_titles
+  $RSS_URLS.each do |url|
+    open(url) do |rss|
+      feed = RSS::Parser.parse(rss)
+      titles = feed.items.map(&:title)
+      notified_titles = titles - $CACHE[url]
+      $CACHE[url] |= titles
+      Libnotify.show(:summary => feed.channel.title,
+                     :body => notified_titles.join("\n"),
+                     :timeout => $TIMEOUT,
+                     :icon_path => $ICON_PATH
+                    ) unless notified_titles.empty?
+    end
+    sleep $TIMEOUT
+  end
+end
+
+show_rss_titles
+
+EventMachine::run do
+  EventMachine::PeriodicTimer.new($INTERVAL) do
+    show_rss_titles
+  end
+end
