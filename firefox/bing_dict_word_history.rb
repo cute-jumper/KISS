35a107ec00693fa911fd5dac4c236d11f87c24f3
Junpeng Qiu <qjpchmail@gmail.com>
2017-02-12 15:21:25 -0500

    Read bing dict search history from firefox

---
+++ firefox/bing_dict_word_history.rb
@@ -0,0 +1,23 @@
+#! /usr/bin/env ruby
+#-*- coding: utf-8 -*-
+# Author: Junpeng Qiu
+# Date: <2017-02-12 Sun>
+# Description: Read search history for bing dict
+
+require 'uri'
+require 'set'
+require 'sqlite3'
+
+$DB_FILE = Dir.glob("#{File.expand_path('~/.mozilla/firefox')}/*default/places.sqlite")[0]
+
+db = SQLite3::Database.new $DB_FILE
+
+rows =
+  db.execute('select url from moz_places where url like "%bing.com/dict/search?%";')
+rows = rows.map do |r|
+  URI.unescape(/.*bing.com\/.*[\?&]q=(.*?)(?=&|$)/.match(r[0])[1]).strip.
+    gsub('+' ,' ');
+end.reject do |r|
+  r.empty? or (not r.ascii_only?)
+end.to_set.to_a
+puts rows
